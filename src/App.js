1a65e0f369f9347ce7fbf2bddd4a45fb1438fb35
max bischof <max@bischof.dk>
2020-08-22 09:56:00 +0200

    Move header component to layout hoc

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import {
-  Switch,
-  Route,
-  useLocation,
-} from 'react-router-dom'
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import LocationPage from './components/pages/locations/LocationPage'
 import EpisodesPage from './components/pages/episodes/EpisodesPage'
 import CharactersPage from './components/pages/characters/CharactersPage'
 import styled from 'styled-components'
 import Game from './components/pages/game/GamePage'
-import Header from './components/Header'
 import Layout from './components/Layout'
 
 export default function App() {
-  const [headerHeadline, setHeaderHeadline] = useState('Rick and Morty')
-
-  const location = useLocation()
-
-  useEffect(() => {
-    let headline = capitalizeFirstLetter(location.pathname.substring(1))
-    location.pathname === '/' && (headline = 'Rick and Morty')
-    setHeaderHeadline(headline)
-  }, [location.pathname])
-
   return (
     <div className="App">
-      <Header text={headerHeadline} />
-      <Main>
-        <Switch>
-          <Layout>
+      <Layout>
+        <Main>
+          <Switch>
             <Route path="/locations">
               <LocationPage />
             </Route>
@@ -41,10 +25,12 @@ export default function App() {
             <Route path="/game">
               <Game />
             </Route>
-            <Route path="/"></Route>
-          </Layout>
-        </Switch>
-      </Main>
+            <Route path="/">
+              {/* See Layout.js */}
+            </Route>
+          </Switch>
+        </Main>
+      </Layout>
     </div>
   )
 }
@@ -53,7 +39,3 @@ const Main = styled.main`
   padding-bottom: 70px;
   padding-top: 80px;
 `
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
-}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navigation from './Navigation'
 import { useLocation } from 'react-router-dom'
 import NavigationStartPage from './NavigationStartPage'
+import Header from './Header'
+import capitalizeFirstLetter from './utils/Capitalize'
 
 export default function Layout({ children }) {
-  const location = useLocation()
-  const isStartPage = location.pathname === '/'
+  const location = useLocation().pathname
+  const isStartPage = location === '/'
+
+  const [headerHeadline, setHeaderHeadline] = useState('Rick and Morty')
+
+  useEffect(() => {
+    let headline = capitalizeFirstLetter(location.substring(1))
+    isStartPage && (headline = 'Rick and Morty')
+    setHeaderHeadline(headline)
+  }, [location, isStartPage])
 
   return (
     <>
+      <Header text={headerHeadline} />
       {children}
       {isStartPage ? <NavigationStartPage /> : <Navigation />}
     </>
